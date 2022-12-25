6640aa073de1d657b7047821a80aaa4925c03d49
Sébastien Pradier <sebastienpradier@MacBook-Pro-de-Sebastien.local>
2022-12-25 18:15:46 -0500

    combining chatGPT and dall-E together

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import openai 
+import pandas as pd
+from flask import Flask, request, render_template
+
+# Instantiate the app
+app = Flask(__name__)
+
+openai.api_key = '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'
+
+def chat_gpt(prompt):
+    response = openai.Completion.create(
+      model="text-davinci-003",
+      prompt=prompt,
+      max_tokens=4000,
+      temperature=0.5
+    )
+    print(response['choices'][0]['text'])
+    return response['choices'][0]['text']
+
+def dall_e(prompt):
+    response = openai.Image.create(
+      prompt=prompt,
+      n=1,
+      size="512x512"
+    )
+    image_url = response['data'][0]['url']
+    print(response['data'][0])
+    return image_url
+
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    if request.method == 'GET':
+      return render_template('index.html')
+
+    if request.method == "POST":
+        text = request.form['text']
+        processed_text = text.upper()
+        return render_template('index.html', data=dall_e(chat_gpt(processed_text)))
+
+if __name__ == '__main__':
+    app.run()
